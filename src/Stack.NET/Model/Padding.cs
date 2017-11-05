ce265b7462c3b9307bfb0e994d935aa583d9ee6c
jrbeverly <jrbeverly@live.com>
2017-11-05 18:44:00 -0500

    Adds in a coordinate system

---
+++ src/Stack.NET/Model/Padding.cs
@@ -0,0 +1,21 @@
+﻿namespace Stack.NET.Model
+{
+    public struct Padding
+    {
+        public double Left;
+        public double Right;
+        public double Up;
+        public double Down;
+        public double Forward;
+        public double Backward;
+
+        public double Horizontal => Left + Right;
+        public double Vertical => Up + Down;
+        public double Depth => Forward + Backward;
+
+        public Padding(double value = 0.0)
+        {
+            Left = Right = Up = Down = Forward = Backward = value;
+        }
+    }
+}
---
+++ src/Stack.NET/Model/WorldSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Media.Media3D;
+using Stack.NET.Utility;
+
+namespace Stack.NET.Model
+{
+    public sealed class WorldSystem
+    {
+        public Padding Padding { get; set; }
+        public double Length { get; set; }
+        public double Half => Length / 2.0D;
+
+        public Point3D Get(Index3D position)
+        {
+            return new Point3D
+            {
+                X = position.X * (Length + Padding.Horizontal) + Padding.Left,
+                Y = position.Y * (Length + Padding.Vertical) + Padding.Down,
+                Z = position.Z * (Length + Padding.Depth) + Padding.Backward
+            };
+        }
+
+        public Point3D Center(Index3D position)
+        {
+            return new Point3D
+            {
+                X = position.X * (Length + Padding.Horizontal) + Padding.Left + Half,
+                Y = position.Y * (Length + Padding.Vertical) + Padding.Down + Half,
+                Z = position.Z * (Length + Padding.Depth) + Padding.Backward + Half
+            };
+        }
+    }
+}
