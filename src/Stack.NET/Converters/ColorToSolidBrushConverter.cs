db897a121345c288d240e38321390c9d3f2697b7
jrbeverly <jrbeverly@live.com>
2017-11-05 17:45:14 -0500

    Polishes the brush converter

--- src/Stack.NET/Converters/ColorToSolidBrushConverter.cs
+++ src/Stack.NET/Converters/ColorToSolidBrushConverter.cs
@@ -7,17 +7,11 @@ namespace Stack.NET.Converters
 {
     /// <inheritdoc />
     /// <summary>
-    /// An implementation of <see cref="T:System.Windows.Data.IValueConverter" /> that converts
-    /// <see cref="T:System.Drawing.Color" /> values to <see cref="T:System.Windows.Media.Color" /> values.
+    /// An implementation of <see cref="T:System.Windows.Data.IValueConverter" /> that converts <see cref="T:System.Drawing.Color" /> values to <see cref="T:System.Windows.Media.Color" /> values.
     /// </summary>
     public class ColorToSolidBrushConverter : IValueConverter
     {
-        /// <summary>Attempts to convert the specified value.</summary>
-        /// <param name="value">The value to convert.</param>
-        /// <param name="targetType">The type of the binding target property.</param>
-        /// <param name="parameter">The converter parameter to use.</param>
-        /// <param name="culture">The culture to use in the converter.</param>
-        /// <returns>A converted value.</returns>
+        /// <inheritdoc />
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (null == value)
@@ -27,15 +21,10 @@ namespace Stack.NET.Converters
                 return new SolidColorBrush(color);
 
             var type = value.GetType();
-            throw new InvalidOperationException("Unsupported type [" + type.Name + "]");
+            throw new InvalidOperationException($"Unsupported type [{type.Name}]");
         }
 
-        /// <summary>Attempts to convert the specified value back.</summary>
-        /// <param name="value">The value to convert.</param>
-        /// <param name="targetType">The type of the binding target property.</param>
-        /// <param name="parameter">The converter parameter to use.</param>
-        /// <param name="culture">The culture to use in the converter.</param>
-        /// <returns>A converted value.</returns>
+        /// <inheritdoc />
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
